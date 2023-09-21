50596a38e9103452bbdda2435212bf0daf4b63d2
minimirror1 <minimirror1@gmail.com>
2023-09-21 19:53:27 +0900

    CANopen loop 내용 추가

--- Task_ZeroErr/Src/main_ZeroErr.cpp
+++ Task_ZeroErr/Src/main_ZeroErr.cpp
@@ -20,6 +20,7 @@
 
 /* User Task -----------------------------------------------------------------*/
 #include "main_ZeroErr.h"
+#include "main_MRS.h"
 
 /* Component -----------------------------------------------------------------*/
 #include "cpp_serial.h"
@@ -36,14 +37,18 @@
 
 /* Private variables ---------------------------------------------------------*/
 extern CO_t* CO;
-
 Motors motors;
+
+extern osMessageQId zerPosiHandle;
 
 /* Private function prototypes -----------------------------------------------*/
 void CANopenNode_Init(void);
 
 
 void main_ZeroErr(void *argument){
+
+
+	MotionPacket_TypeDef motionMsg;
 
 	/* CANopen Init */
 	CANopenNode_Init();
@@ -54,24 +59,31 @@ void main_ZeroErr(void *argument){
 
 	motors.motorsInit(CO, 1, 12);
 
-	motors.add_motor(1, ROT_CW  ,90  ,262144, 262144);
-	motors.add_motor(2, ROT_CCW ,90  ,174763, 174763);
-	motors.add_motor(3, ROT_CW  ,60  ,174763, 174763);
-	motors.add_motor(4, ROT_CW  ,130 ,174763, 174763);
+	motors.add_motor(1 , ROT_CW  ,90  ,262144, 262144);
+	motors.add_motor(2 , ROT_CCW ,90  ,174763, 174763);
+	motors.add_motor(3 , ROT_CW  ,60  ,174763, 174763);
+	motors.add_motor(4 , ROT_CW  ,130 ,174763, 174763);
 
-	motors.add_motor(5,  ROT_CCW ,210 ,262144, 262144);
-	motors.add_motor(6,  ROT_CW  ,160 ,262144, 262144);
-	motors.add_motor(7,  ROT_CCW ,60  ,262144, 262144);
-	motors.add_motor(8,  ROT_CCW ,40  ,262144, 262144);
+	motors.add_motor(5 , ROT_CCW ,210 ,262144, 262144);
+	motors.add_motor(6 , ROT_CW  ,160 ,262144, 262144);
+	motors.add_motor(7 , ROT_CCW ,60  ,262144, 262144);
+	motors.add_motor(8 , ROT_CCW ,40  ,262144, 262144);
 
-	motors.add_motor(9,  ROT_CW  ,210 ,262144, 262144);
+	motors.add_motor(9 , ROT_CW  ,210 ,262144, 262144);
 	motors.add_motor(10, ROT_CCW ,160 ,262144, 262144);
 	motors.add_motor(11, ROT_CW  ,60  ,262144, 262144);
 	motors.add_motor(12, ROT_CW  ,40  ,262144, 262144);
 
-
-
 	while(1){
+
+		osStatus_t status = osMessageQueueGet(zerPosiHandle, &motionMsg, NULL, 0U); // wait for message
+		if (status == osOK) {
+			motors.setPosition(motionMsg.sid, motionMsg.posi);
+		}
+
+		motors.movePosition();
+		send_RPDO_1_BuffSend(CO);
+		send_sync(CO);
 		osDelay(10);
 	}
 }
