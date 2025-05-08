3c8891ff6fe8f728c98fe7be260c3f164131bd63
minimirror1 <minimirror1@gmail.com>
2025-05-08 17:47:52 +0900

    불필요한 주석 및 함수 제거: ZER_Init_Process 및 zer_statusFaultCheck 함수와 관련된 주석을 삭제하고, main_ZeroErr 함수 내에서 호출되는 관련 코드 정리. 코드 가독성 향상 및 유지보수 용이성 증대.

--- Task_ZeroErr/Src/main_ZeroErr.cpp
+++ Task_ZeroErr/Src/main_ZeroErr.cpp
@@ -69,13 +69,6 @@ uint32_t os_rx_cnt = 0;
 void ZER_Init_Process(void);
 void CANopenNode_Init(void);
 
-/* 통신 처리 관련 함수 */
-void mrs_zerrx_cmd_process(BypassPacket_TypeDef *cmd_rx);
-void zer_statusFaultCheck(void);
-
-
-
-
 void CANopenNode_Init(void){
     canOpenNodeSTM32.CANHandle = &hcan2;
     canOpenNodeSTM32.HWInitFunction = MX_CAN2_Init;
@@ -87,8 +80,6 @@ void CANopenNode_Init(void){
 
 void main_ZeroErr(void *argument){
     /* CANopen Init */
-    //CANopenNode_Init();
-
 	CANopenNode_Init();
     TPDO1_rx_init();
 
@@ -97,196 +88,16 @@ void main_ZeroErr(void *argument){
 
     motors.motorsInit(CO, 1, 30);
 
-    //motors.init_status_led(LD_ZER_ERR_GPIO_Port, LD_ZER_ERR_Pin, GPIO_PIN_RESET);
-//    motors.add_motor(
-//        3 ,
-//        ROT_CW  ,
-//        359  ,
-//        174763,
-//        174763,
-//        0);
-//
-//    motors.add_motor(
-//        4 ,
-//        ROT_CW  ,
-//        359  ,
-//        174763,
-//        174763,
-//        0);
-//
-//    motors.init_motor(3);
-//    motors.init_motor(4);
-//
-//	motors.init_default_posi(3);
-//	motors.init_default_posi(4);
-
-    //Zer_All_init_flag = INIT_INFO_DEFAULT_POSI_START;
-
     while(1){
 #ifdef CANOPEN_MODE
         mrsComm.processCommandQueue();
         mrsComm.processPositionQueue();
         mrsComm.checkCommunicationStatus();
         mrsComm.processMovePosition();
-        //ZER_Init_Process();
 
         motors.default_posi_check_process();
 #endif
     }
-}
-
-void ZER_Init_Process(void){
-
-	if(Zer_All_init_flag == INIT_INFO_DEFAULT_POSI_START){
-		uint8_t init_result = motors.init();
-		if(init_result == 1)				//성공
-			Zer_All_init_flag = INIT_OK;	//init ok
-		else if(init_result == 0){			//실패
-			Zer_All_init_flag = INIT_FAIL;	//init fail
-			osDelay(1);
-			send_sync(CO);
-			zer_statusFaultCheck();
-		}
-	}
-	else if(Zer_All_init_flag == INIT_OK){
-		motors.movePosition();
-		send_RPDO_BuffSend(CO);
-		osDelay(1);
-		send_sync(CO);
-		//zer_statusFaultCheck();
-	}
-	else if(Zer_All_init_flag == INIT_DEFAULT_POSI_START){
-		osDelay(3000);// 이후 초기위치 이동은 3초 후 시작한다.
-		uint8_t def_result = motors.default_posi();
-		if(def_result == 1)				//성공
-			Zer_All_init_flag = INIT_OK;	//init ok
-		else if(def_result == 0)			//실패
-			Zer_All_init_flag = INIT_FAIL;	//init fail
-	}
-}
-
-void zer_statusFaultCheck(void){
-
-	for(int i = 1; i <= 12; i++){
-		ZER_StatusCheck_TypeDef status = motors.getStatusFaultCheck(i);
-		if(status != 999 && status != ZER_STATUS_NONE) {
-
-			if(t_statusFault[i].delay(500) && zerErrorTxCnt != 0){
-				char str[10] = {0,};
-				if(status == ZER_STATUS_FAULT){
-					strcpy(str, "001");	//fault
-				}
-				else if(status == ZER_STATUS_WARNING){
-					strcpy(str, "002");//warning
-				}
-				else if(status == ZER_STATUS_TIMEOUT){
-					strcpy(str, "003");//timeout
-				}
-
-
-				BypassPacket_TypeDef msg = {0,};
-				msg.gid = zergid;
-				msg.sid = i;
-				msg.cmd = MRS_TX_ERROR_MSG;
-				memcpy(msg.data, (uint8_t *)str, 8);
-				osMessageQueuePut(zerCmd_txHandle, &msg, 0U, 0U);
-				Zer_All_init_flag = INIT_FAIL;
-				zerErrorTxCnt--;
-			}
-		}
-	}
-}
-
-void mrs_zerrx_cmd_process(BypassPacket_TypeDef *cmd_rx) {
-
-	if (cmd_rx->sid > 12)
-		return;
-
-	zergid = cmd_rx->gid;//240305;
-
-	switch (cmd_rx->cmd) {
-	case MRS_RX_DATA1: {
-		prtc_data_ctl_init_driver_data1_t *pData = (prtc_data_ctl_init_driver_data1_t*) cmd_rx->data;
-
-		zerSetting[cmd_rx->sid].rot_dir = (pData->direction == 0 ? ROT_CW : ROT_CCW);
-		zerSetting[cmd_rx->sid].angle = (float) pData->angle / 100;
-		zerSetting[cmd_rx->sid].defult_posi = pData->init_position;
-		zerSetting[cmd_rx->sid].f_data1 = true;
-
-
-		BypassPacket_TypeDef msg;
-		msg.gid = cmd_rx->gid;
-		msg.sid = cmd_rx->sid;
-		msg.cmd = MRS_TX_DATA1_ACK;
-		memcpy(msg.data, (uint8_t *)pData, 8);
-		osMessageQueuePut(zerCmd_txHandle, &msg, 0U, 0U);
-
-		break;
-	}
-
-	case MRS_RX_DATA_OP: {
-			if (zerSetting[cmd_rx->sid].f_data1 != true)
-				return;
-			prtc_data_ctl_init_driver_data_op_zero_err_t *pData = (prtc_data_ctl_init_driver_data_op_zero_err_t*) cmd_rx->data;
-			zerSetting[cmd_rx->sid].tar_speed = pData->profile_target_speed;
-			zerSetting[cmd_rx->sid].tar_acc = pData->profile_acc_cnt;
-
-			motors.add_motor(
-					cmd_rx->sid,
-					zerSetting[cmd_rx->sid].rot_dir ,
-					zerSetting[cmd_rx->sid].angle ,
-					zerSetting[cmd_rx->sid].tar_speed,
-					zerSetting[cmd_rx->sid].tar_acc,
-					zerSetting[cmd_rx->sid].defult_posi);
-
-			if( motors.init_motor(cmd_rx->sid) == 1){
-				Zer_All_init_flag = INIT_OK;
-			}
-
-			BypassPacket_TypeDef msg;
-			msg.gid = cmd_rx->gid;
-			msg.sid = cmd_rx->sid;
-			msg.cmd = MRS_TX_DATA_OP_ACK;
-			memcpy(msg.data, (uint8_t *)pData, 8);
-			osMessageQueuePut(zerCmd_txHandle, &msg, 0U, 0U);
-
-			break;
-		}
-
-
-	case MRS_RX_MOVE_DEFAULT_POSI: {
-
-		if(cmd_rx->sid == 12){
-			//new data
-			if(zerSetting[cmd_rx->sid].f_data1 == true){
-				Zer_All_init_flag = INIT_INFO_DEFAULT_POSI_START;
-				zerSetting[cmd_rx->sid].f_data1 = false; //f_data1 가 먼저 수신된 후 data2가 수신되었을때만 유효하다.
-			}
-			else{ //default posi only
-				Zer_All_init_flag = INIT_DEFAULT_POSI_START;
-			}
-		}
-		break;
-	}
-
-	case MRS_RX_MOVE_DEFAULT_POSI_CHECK: {
-		if(Zer_All_init_flag == INIT_OK){
-			BypassPacket_TypeDef msg = {0,};
-			msg.gid = cmd_rx->gid;
-			msg.sid = cmd_rx->sid;
-			msg.cmd = MRS_TX_MOVE_DEFAULT_POSI_CHECK;
-			osMessageQueuePut(zerCmd_txHandle, &msg, 0U, 0U);
-		}
-		break;
-	}
-	case MRS_TX_MOTOR_STATUS_CHECK :{
-		//240729
-
-		break;
-	}
-	default:
-		break;
-	}
 }
 
 void HAL_CAN_RxFifo0MsgPendingCallback_ByPass(CAN_HandleTypeDef *hcan)//231110 shs
